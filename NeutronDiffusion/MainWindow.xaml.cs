c2c1773bd7cc0f4dd748297c9aa215bb92fd15a8
Анатолий Мальцев <malcevanatoly@gmail.com>
2015-12-26 10:24:52 +0300

    hot fixes + integrated theory

--- NeutronDiffusion/MainWindow.xaml.cs
+++ NeutronDiffusion/MainWindow.xaml.cs
@@ -40,6 +40,10 @@ namespace NeutronDiffusion
             InitMaterialList();
             InitSimulateOneTab();
             InitSimulateBatchTab();
+            if (!File.Exists("theory.html"))
+                return;
+            string curDir = Directory.GetCurrentDirectory();
+            wb.Source = new Uri(String.Format("file:///{0}/theory.html", curDir));
         }
 
         public class Material
@@ -536,6 +540,33 @@ namespace NeutronDiffusion
                 number = getDefaultNumber();
             }
             return number;
+        }
+    }
+
+    public static class BrowserBehavior
+    {
+        public static readonly DependencyProperty HtmlProperty = DependencyProperty.RegisterAttached(
+            "Html",
+            typeof(string),
+            typeof(BrowserBehavior),
+            new FrameworkPropertyMetadata(OnHtmlChanged));
+
+        [AttachedPropertyBrowsableForType(typeof(WebBrowser))]
+        public static string GetHtml(WebBrowser d)
+        {
+            return (string)d.GetValue(HtmlProperty);
+        }
+
+        public static void SetHtml(WebBrowser d, string value)
+        {
+            d.SetValue(HtmlProperty, value);
+        }
+
+        static void OnHtmlChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            WebBrowser wb = d as WebBrowser;
+            if (wb != null)
+                wb.NavigateToString(e.NewValue as string);
         }
     }
 
